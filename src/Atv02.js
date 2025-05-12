8d5c4becb67f093afc9061669d7bb21edc727011
Kamilyszg <kamily.gracia@gmail.com>
2025-05-12 19:15:32 +0000

    "Feat: Atividade Contador"

--- src/Atv02.js
+++ src/Atv02.js
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import Contador from "./Atividade-03/Contador";
+
 
 export default function Atv02(){
     return (
         <>
-            <h2>Semana 2</h2> 
-            <Link to="/">Voltar</Link>       
+            <h2>Semana 2</h2>
+            <Contador /> 
+
+            <Link to="/" >Voltar</Link>       
         </>
     );
 };
